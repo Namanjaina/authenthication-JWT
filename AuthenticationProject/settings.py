55d59c8acba2f30c5d35881dd540334955d2af93
Naman Bagrecha <120776897+Namanjaina@users.noreply.github.com>
2026-03-20 20:15:30 +0530

    Change EMAIL_HOST to environment variable
    Updated EMAIL_HOST to use environment variable.

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -108,7 +108,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- EMAIL CONFIG (Recommended Port 587 for Render) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
