cae544232b2c3720d8619b4df1180caad1f4699d
Naman Bagrecha <120776897+Namanjaina@users.noreply.github.com>
2026-03-20 19:44:04 +0530

    Update settings.py

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -12,8 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 
-# IMPORTANT: Render Environment Variables mein DEBUG ki value 'false' (small letters) rakhein
-DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*'] 
 
