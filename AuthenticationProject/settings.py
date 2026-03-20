43c9ea4d6ad48d701c09cee5910c1e31c21c43b1
Naman Bagrecha <120776897+Namanjaina@users.noreply.github.com>
2026-03-20 20:05:23 +0530

    Update settings.py

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -109,9 +109,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --- EMAIL CONFIG (Recommended Port 587 for Render) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
